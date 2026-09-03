e145717f97076e552d6e9999e0daf219c2fb9c05
jagg26 <john.ale26@gmail.com>
2026-09-02 21:56:22 -0300

    feat(code): add week 1 code functions

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        // First, I create a fixed-size array of type double using the length provided by length.
 
-        return []; // replace this return statement with your own
+        var fixedArr = new double[length];
+
+        // Then, I iterate through each position in the array, starting at index 0 and ending at length - 1.
+
+        
+        for (int i = 0; i < length; i++)
+        {
+            // At each position, I store the value of number multiplied by i + 1, because array indexes start at 0, but the multiples should start at 1.
+            fixedArr[i] = number * (i + 1);
+        }
+
+        return fixedArr; // Finally, I return the array containing all the calculated multiples.
     }
 
     /// <summary>
@@ -29,5 +42,40 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+    // Find the index where the last amount elements begin.
+    
+    int startIndex = data.Count - amount;
+
+
+    // Create a temporary list where to store
+    // the elements in their new rotated order.
+    
+    List<int> rotated = new List<int>();
+
+
+    // Copy the last "amount" elements to the beginning of the temporary array.
+    
+    for (int i = startIndex; i < data.Count; i++)
+    {
+        rotated.Add(data[i]);
+    }
+
+
+    // Copy the elements that were originally at the beginning and they will be now at the end of the temporary array
+    
+    for (int i = 0; i < startIndex; i++)
+    {
+        rotated.Add(data[i]);
+    }
+
+    // Replace the contents of the original list with the elements from the rotated list.
+    
+    data.Clear();
+
+    for (int i = 0; i < rotated.Count; i++)
+    {
+        data.Add(rotated[i]);
+    }
     }
 }
